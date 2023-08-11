542b07ddf24f59f34bcbefb60b54a2f84871de52
Akojede70 <peter.oluwamayowa.akojede@gmail.com>
2023-08-11 12:26:49 +0100

    work on the book page and syllabus

--- school/src/Pages/Sub.jsx
+++ school/src/Pages/Sub.jsx
@@ -4,41 +4,64 @@ import { IoIosCheckmarkCircle } from "react-icons/Io";
 
 const sub = () => {
   return (
-    <div className="w-full md:h-[550px] pt-[50px] bg-[#fff3d8] px-4 md:px-6 lg:px-10">
-      <div className="w-full md:flex md:justify-evenly">
-        <div className="md:text-none text-center">
-          <h2 className="md:text-5xl font-bold">Browse Through Jubep Syllabus</h2>
-          <p  className="pt-[20px] md:text-xl">
+    <div className="w-full md:h-[550px]">
+      {/* front page */}
+      <div className="w-full bg-[#fff3d8] md:flex md:justify-evenly">
+        <div className="md:text-none text-center pt-[60px]">
+          <h2 className="md:text-5xl text-2xl font-bold">
+            Browse Through Jubep Syllabus
+          </h2>
+          <p className="pt-[20px] md:text-xl">
             This is the official Jupeb Government syllabus which contain
             001/002/003/ & 004 <br></br>
             and which the Jupeb textbook follow the arrangement in the syllabus.
           </p>
 
-          <img className="h-[350px] md:hidden block mx-auto my-auto" src={img} alt="book" />
+          <img
+            className="h-[350px] md:hidden block mx-auto my-auto md:mt-0 mt-[60px]"
+            src={img}
+            alt="book"
+          />
 
-          <div className="text-center ml-[120px] mt-[50px] text-xl">
-            <div className="flex">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]"/> Your Passport to Excellence
-            </div>
-            <p className="flex">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]"/> Open Access & premium content
+          <div className="w-full sm:pl-[120px] pl-[20px] mt-[20px] text-xl">
+            <p className="flex  items-center">
+              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
+              Your Passport to Excellence
             </p>
-            <p className="flex">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]"/> Your Roadmap to Success
+            <p className="flex  items-center">
+              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
+              Open Access & premium content
             </p>
+            <p className="flex items-center">
+              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
+              Your Roadmap to Success
+            </p>
+            <p className="underline mt-[30px] text-xl md:pb-0 pb-[80px] pl-[10px] cursor-pointer text-left"><a href="/"></a>Download government syllabus</p>
           </div>
-
-          <button className="text-center underline  mt-[30px] text-xl mx-auto my-auto">
-            <a href="/" download>
-              Download government syllabus
-            </a>
-          </button>
         </div>
+        
 
-        <div className="mb-[120px]">
+        <div className="my-[60px]">
           <img className="h-[500px] hidden md:block" src={img} alt="book" />
         </div>
       </div>
+
+      <section>
+        <div className=" w-full md:grid md:grid-cols-4 md:grid-rows-4 md:space-x-[10px] space-y-[30px] grid grid-cols-2 grid-rows-2  pt-[50px]">
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+          <img className="h-[350px]" src={img} alt="book" />
+        </div>
+      </section>
     </div>
   );
 };
