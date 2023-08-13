94c2caaa437c4004da0729f977e6db954703d5d0
Samuel-Makinde <makindesamuel1999@gmail.com>
2023-08-13 08:04:15 +0100

    fix image size

--- JupepProject/src/components/ImageSlider/ImageSlider.jsx
+++ JupepProject/src/components/ImageSlider/ImageSlider.jsx
@@ -1,14 +1,15 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import banner from "../../assets/Home Landing Image/Jupeb banner 1.jpg";
-import science from "../../assets/Home Landing Image/jupeb hard cover-science & management science.jpg";
-import artPQ from "../../assets/Home Landing Image/jupeb hard cover ART  & SOCAIL SCIENCE.jpg";
-import management from "../../assets/Home Landing Image/jupeb hard cover management.jpg";
-import unilag from "../../assets/Home Landing Image/UNILAG standing.jpg";
-import oau from "../../assets/Home Landing Image/oau standing.jpg";
-import unilorin from "../../assets/Home Landing Image/unilorin standing.jpg";
-import futa from "../../assets/Home Landing Image/futa hard cover.jpg";
+// import banner from "../../assets/Home Landing Image/Jupeb banner 1.jpg";
+// import science from "../../assets/Home Landing Image/jupeb hard cover-science & management science.jpg";
+// import artPQ from "../../assets/Home Landing Image/jupeb hard cover ART  & SOCAIL SCIENCE.jpg";
+// import management from "../../assets/Home Landing Image/jupeb hard cover management.jpg";
+// import unilag from "../../assets/Home Landing Image/UNILAG standing.jpg";
+// import oau from "../../assets/Home Landing Image/oau standing.jpg";
+// import unilorin from "../../assets/Home Landing Image/unilorin standing.jpg";
+import futa from "../../assets/Home Landing Image/new futa.jpg";
+import Nfuta from "../../assets/Home Landing Image/large futa.jpg";
 import styles from "./ImageSlider.module.css";
 
 const ImageSlider = () => {
@@ -29,7 +30,7 @@ const ImageSlider = () => {
         <div className="relative w-full h-full border-none font-euclid">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full   border-none ${styles.imageContainer}`}
-            src={banner}
+            src={futa}
             alt="banner"
           />
           <div className="absolute w-full top-[80px] flex items-center justify-center ">
@@ -41,49 +42,49 @@ const ImageSlider = () => {
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={science}
+            src={Nfuta}
             alt="scinece"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full   border-none ${styles.imageContainer}`}
-            src={artPQ}
+            src={futa}
             alt="artPQ"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={management}
+            src={Nfuta}
             alt="management"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={unilag}
+            src={futa}
             alt="unilag"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={oau}
+            src={Nfuta}
             alt="oau"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={unilorin}
+            src={futa}
             alt="unilorin"
           />
         </div>
         <div className="w-full h-full border-none">
           <img
             className={`rounded-[30px] md:rounded-none md:w-full  border-none ${styles.imageContainer}`}
-            src={futa}
+            src={Nfuta}
             alt="futa"
           />
         </div>
