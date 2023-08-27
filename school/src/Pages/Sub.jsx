c5d067eab29448015d58c3e4aa47af6fa33f2a87
Akojede70 <peter.oluwamayowa.akojede@gmail.com>
2023-08-28 00:04:56 +0100

    The syllabus file

--- school/src/Pages/Sub.jsx
+++ school/src/Pages/Sub.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import img from "../Pages/img.png";
-// import { IoIosCheckmarkCircle } from "react-icons/Io";
+import syllabus from "./data";
 
 const sub = () => {
   return (
-    <div className="w-full md:h-[550px]">
+    <div className="w-full md:h-[550px] bg-[#fff3d8]">
       {/* front page */}
 
-      <div className="text-center bg-[#fff3d8] pt-[50px]">
-          <h2 className="md:text-5xl text-2xl font-bold">
-            Browse Through Jubep Syllabus
-          </h2>
-          <p className="pt-[20px] text-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-          </div>
-      <div className="w-full md:flex md:justify-evenly bg-[#fff3d8]">
+      <div className="text-center pt-[50px]">
+        <h2 className="md:text-5xl text-2xl font-bold">
+          Browse Through Jubep Syllabus
+        </h2>
+        <p className="pt-[20px] text-2xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing.
+        </p>
+      </div>
+      <div className="w-full md:flex md:justify-evenly">
         <div className="md:text-none text-center pt-[60px]">
           <h3 className="text-4xl font-bold">Jupeb Government Syllabus</h3>
           <p className="pt-[20px] md:text-xl">
@@ -21,53 +23,49 @@ const sub = () => {
             001/002/003/ & 004 <br></br>
             and which the Jupeb textbook follow the arrangement in the syllabus.
           </p>
-          <p className="underline mt-[30px] text-xl md:pb-0 pb-[80px] pl-[10px] cursor-pointer "><a href="/"></a>Download government syllabus</p>
+          <p className="underline mt-[30px] text-xl md:pb-0 pb-[80px] pl-[10px] cursor-pointer">
+            <a href="/"></a>Download government syllabus
+          </p>
 
           <img
             className="h-[400px] md:hidden block mx-auto my-auto md:mt-0 mt-[10px]"
             src={img}
             alt="book"
           />
-
-          {/* <div className="w-full sm:pl-[120px] pl-[20px] mt-[20px] text-xl">
-            <p className="flex  items-center">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
-              Your Passport to Excellence
-            </p>
-            <p className="flex  items-center">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
-              Open Access & premium content
-            </p>
-            <p className="flex items-center">
-              <IoIosCheckmarkCircle size={20} className="mr-[10px] mt-[6px]" />{" "}
-              Your Roadmap to Success
-            </p>
-            <p className="underline mt-[30px] text-xl md:pb-0 pb-[80px] pl-[10px] cursor-pointer text-left"><a href="/"></a>Download government syllabus</p>
-          </div> */}
         </div>
-        
 
-        <div className="my-[60px]">
-          <img className="h-[350px] hidden md:block" src={img} alt="book" />
+        <div className="pt-[40px]">
+          <img className=" h-[350px] hidden md:block" src={img} alt="book" />
         </div>
       </div>
 
-      {/* <section>
-        <div className=" w-full md:grid md:grid-cols-4 md:grid-rows-4 md:space-x-[10px] space-y-[30px] grid grid-cols-2 grid-rows-2  pt-[50px]">
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-          <img className="h-[350px]" src={img} alt="book" />
-        </div>
-      </section> */}
+      <section className="bg-[#fff3d8] pt-[30px]">
+        
+          {syllabus.map((item) => (
+            <div key={item.id} className="w-full  md:flex md:justify-evenly bg-[#fff3d8]">
+              <div className="md:text-none text-center">
+                <h2 className="text-4xl font-bold">{item.title}</h2>
+                <p className="pt-[20px] md:text-xl md:pl-20 text-center">{item.description}</p>
+                <p className="underline mt-[30px] text-xl md:pb-0 pb-[80px] pl-[10px] cursor-pointer">
+                  {item.download}
+                </p>
+                <img
+                className="h-[400px] md:hidden block mx-auto my-auto md:pt-0 pt-[10px]"
+                src={item.img}
+                alt={item.picture}
+              />
+              </div>
+
+              <div className="my-[20px] w-[800px] ml-[80px]">
+                <img
+                  className="h-[350px] hidden md:block"
+                  src={item.img}
+                  alt={item.picture}
+                />
+              </div>
+            </div>
+          ))}
+      </section>
     </div>
   );
 };
--- school/src/Pages/data.jsx
+++ school/src/Pages/data.jsx
@@ -1,10 +1,85 @@
-// const syllabus = [
-//     {
-//         id: 1,
-//         Title: ,
-//         img: 
-//         Description: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, dignissimos.
-//     }
-// ]
+import img from '../Pages/img.png';
+const syllabus = [
+    {
+        id: 1,
+        title: "Jupeb Mathematics Syllabus", 
+        description: "This is the official Jupeb Mathematics syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Mathematics syllabus"
+    },
+    {
+        id: 2,
+        title: "Jupeb Physics Syllabus", 
+        description: "This is the official Jupeb Physics syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Physics syllabus"
+    },
+    {
+        id: 3,
+        title: "Jupeb Chemistry Syllabus", 
+        description: "This is the official Jupeb chemistry syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download chemistry syllabus"
+    },
+    {
+        id: 4,
+        title: "Jupeb Biology Syllabus", 
+        description: "This is the official Jupeb Biology syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Biology syllabus"
+    },
+    {
+        id: 5,
+        title: "Jupeb Lit in English Syllabus", 
+        description: "This is the official Jupeb Lit in English syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Lit in English syllabus"
+    },
+    {
+        id: 6,
+        title: "Jupeb Crs Syllabus", 
+        description: "This is the official Jupeb Crs syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img,
+        download: "download crs syllabus"
+    },
+   
+    {
+        id: 7,
+        title: "Jupeb Economics Syllabus", 
+        description: "This is the official Jupeb Economics syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Economics syllabus"
+    },
+    {
+        id: 8,
+        title: "Jupeb Accounting Syllabus", 
+        description: "This is the official Jupeb Accounting syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Accounting syllabus"
+    },
+    {
+        id: 9,
+        title: "Jupeb Business study Syllabus", 
+        description: "This is the official Jupeb business study syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Business study syllabus"
+    },
+    
+    {
+        id: 10,
+        title: "Jupeb History Syllabus", 
+        description: "This is the official Jupeb History syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download History syllabus"
+    },
+    {
+        id: 11,
+        title: "Jupeb Visual Art", 
+        description: "This is the official Jupeb Visual Art syllabus which contain 001/002/003/ & 004 and which the Jupeb textbook follow the arrangement in the syllabus.",
+        img: img, 
+        download: "download Visual Art syllabus"
+    },
+    
+];
 
-// export default syllabus;
+export default syllabus;
